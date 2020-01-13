52e50d7955f091075dbf87b375684b4607829d34
Cherkesky <guy@cherkesky.com>
2020-01-13 16:39:01 -0600

    completed exercises 7

--- zoo.py
+++ zoo.py
@@ -1,17 +1,20 @@
 zoo =("dog","cat","spider","cockroach","fish","ant","bird","peacock","snake","monkey")
-# print (zoo.index("fish"))
+print (zoo.index("fish"))
 
-# animal_to_find = "snake"
-# if animal_to_find in zoo:
-#   print(f'{animal_to_find} was found')
+animal_to_find = "snake"
+if animal_to_find in zoo:
+  print(f'{animal_to_find} was found')
 
 (animal1,animal2,animal3,animal4,animal5,animal6,animal7,animal8,animal9,animal10) = zoo
 
-# print(animal5)
+print(animal5)
 
 zoolist=list(zoo)
 moreAnimals = ["bat","tiger","rabbit"]
 zoolist.extend(moreAnimals)
 print(zoolist)
+
+zootuple=tuple(zoolist)
+print (zootuple)
 
 
